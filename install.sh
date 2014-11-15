20f31828ac13fc48fba0e32166b2172260e5b2f3
毛林 <maolin@shanlitech.com>
2014-11-16 05:53:33 +0800

    add ln .vim .vimrc in install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+vimrc=$PWD
+
 if [ -d ~/.vim ]; then
 	mv ~/.vim ~/.vim-backup
 fi
@@ -12,3 +14,6 @@ vim +BundleInstall +qall
 cd bundle/YouCompleteMe
 ./install.sh --clang-completer
 
+cd ~
+ln -s $vimrc ~/.vim
+ln -s $vimrc/vimconf ~/.vimrc
