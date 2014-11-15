8cb343bd117c163b5ac539af2a2cd3998f05a2a8
毛林 <maolin@shanlitech.com>
2014-11-16 06:24:29 +0800

    update install.sh

--- install.sh
+++ install.sh
@@ -1,19 +1,28 @@
 #!/bin/sh
 
-vimrc=$PWD
+current=$PWD
+vimdir="$HOME/.vim"
 
 if [ -d ~/.vim ]; then
-	mv ~/.vim ~/.vim-backup
+	if [ "$current" != "$vimdir" ]; then
+		echo "backup ~/.vim to ~/.vim-backup"
+		mv ~/.vim ~/.vim-backup
+		ln -s $current ~/.vim
+	fi
+else
+	ln -s $current ~/.vim
 fi
 
 if [ -f ~/.vimrc ]; then
+	echo "backup ~/.vimrc to ~/.vimrc-backup"
 	mv ~/.vimrc ~/.vimrc-backup
 fi
+
+git submodule init
+git submodule update
 
 vim +BundleInstall +qall
 cd bundle/YouCompleteMe
 ./install.sh --clang-completer
 
 cd ~
-ln -s $vimrc ~/.vim
-ln -s $vimrc/vimconf ~/.vimrc
